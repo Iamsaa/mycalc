77272169313002deea9ed3d81a59b41bed3cc0e6
iamsaa <alymarianop@gmail.com>
2023-02-04 15:01:21 +0800

    Update now

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import './App.css';
-import { useState } from 'react';
-function App({label}) {
-  const shoot = () => {
-    value(label);
-  }
-  return (
+import React, { useState } from 'react';
 
-    <button class="btn" onClick={shoot}>  {label}</button>
+function App({ label, onClick }) {
+  return (
+ 
+    <button className="btn" onClick={() => onClick(label)}>
+      {label}
+    </button>
+
     
   );
 }
 
-export default function MyApp(){return (
- 
+export default function MyApp() {
+  const [input, setInput] = useState('');
 
-  <div className="root-container">
-     <input value={label} id="Screen"></input>
-     <div class="btn-body">
-  <App label="7"/>
-<App label="8"/>
-<App label="9"/>
-<App label="/"/><br></br>
-    <App label="4"/> 
-<App label="5"/>
-<App label="6"/>
-<App label="x"/><br></br>
-<App label="1"/>
-<App label="2"/>
-<App label="3"/>
-<App label="-"/><br></br>
-<App label="OR"/>
-<App label="0"/>
-<App label="="/>
-<App label="+"/>
-</div>
-  </div>
-);
+  const clear = () => {
+    setInput('');
+  };
+
+  const handleClick = value => {
+    setInput(input + value);
+  };
+
+  return (
+    <div className="root-container">
+      <input value={input} id="Screen" readOnly />
+      <div className="btn-body">
+        <App label="7" onClick={handleClick} />
+        <App label="8" onClick={handleClick} />
+        <App label="9" onClick={handleClick} />
+        <App label="/" onClick={handleClick} /><br />
+        <App label="4" onClick={handleClick} />
+        <App label="5" onClick={handleClick} />
+        <App label="6" onClick={handleClick} />
+        <App label="x" onClick={handleClick} /><br />
+        <App label="1" onClick={handleClick} />
+        <App label="2" onClick={handleClick} />
+        <App label="3" onClick={handleClick} />
+        <App label="-" onClick={handleClick} /><br />
+        <App label="C" onClick={clear} />
+        <App label="0" onClick={handleClick} />
+        <App label="=" onClick={() => setInput(eval(input))} />
+        <App label="+" onClick={handleClick} />
+      </div>
+    </div>
+  );
 }
-
